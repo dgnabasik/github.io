3f8a927105c607d894764f82173bb65ff6806aeb
dgnabasik <DavidGnabasik@comcast.net>
2024-08-21 10:37:28 -0600

    Release 1.0.2

--- push.sh
+++ push.sh
@@ -1,8 +1,10 @@
-echo dgnabasik
-git pull
+echo dgnabasik in portfolio...
+eval "$(ssh-agent -s)"
+git remote -v
+ssh -T -ai /home/david/.ssh/portfolio_ed25519 git@github.com
+git pull git@github.com:dgnabasik/portfolio.git
 echo -n "push?"
 read
 git add --all :/
-git commit -am "Release 1.0.1"
+git commit -am "Release 1.0.2"
 git push -u origin main
-
